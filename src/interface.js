7511c16e5fe8ca7e39b0aaa8bc27a32f2400920e
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-26 13:41:18 +0100

    JQuery added

---
+++ src/interface.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  updateTemperature();
+
+  $('#temp-up').click(function() {
+    thermostat.increase();
+    updateTemperature();
+  });
+
+  $('#temp-down').click(function() {
+    thermostat.decrease();
+    updateTemperature();
+  });
+
+  $('#temp-reset').click(function() {
+    thermostat.reset();
+    updateTemperature();
+  });
+
+  $('#psm-on').click(function() {
+    thermostat.powerSavingModeOn();
+    $('#power-saving').text('on')
+    updateTemperature();
+  })
+
+  $('#psm-off').click(function() {
+    thermostat.powerSavingModeOff();
+    $('#power-saving').text('off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+  };
+});
